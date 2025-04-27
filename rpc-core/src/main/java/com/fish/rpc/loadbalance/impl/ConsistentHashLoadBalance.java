c6ef16e1fe1b296c883ab8ccef9f0027bb09d999
Afish <2054641315@qq.com>
2025-04-27 11:57:37 +0800

    负载均衡

--- rpc-core/src/main/java/com/fish/rpc/loadbalance/impl/ConsistentHashLoadBalance.java
+++ rpc-core/src/main/java/com/fish/rpc/loadbalance/impl/ConsistentHashLoadBalance.java
@@ -15,7 +15,7 @@ public class ConsistentHashLoadBalance implements LoadBalance {
     @Override
     public String select(List<String> list, RpcReq rpcReq) {
         String key = rpcReq.rpcServiceName();
-        long hashCode = Hashing.murmur3_32().hashString(key, StandardCharsets.UTF_8).asLong();
+        long hashCode = Hashing.murmur3_128().hashString(key, StandardCharsets.UTF_8).asLong();
         int index = Hashing.consistentHash(hashCode, list.size());
         return list.get(index);
     }
